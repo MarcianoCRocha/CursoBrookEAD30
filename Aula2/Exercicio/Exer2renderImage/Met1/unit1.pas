6b1b5be358879b18d7d5461006a652e6c9d1be21
MarcianoCRocha <marcianocrocha@gmail.com>
2014-07-28 22:22:56 -0300

    exercicio 2 metodo 1

---
+++ Aula2/Exercicio/Exer2renderImage/Met1/unit1.pas
@@ -0,0 +1,33 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  BrookAction,BrookHttpUtils, classes, sysutils;
+
+type
+  TMyAction = class(TBrookAction)
+  public
+    procedure Get; override;
+  end;
+
+implementation
+
+procedure TMyAction.Get;
+begin
+  TheResponse.ContentStream := TFileStream.Create( 'D:\Projeto Lazarus\AulaBrook\CursoBrookEAD30\Aula2\Exercicio\Exer2renderImage\images.jpg', fmOpenRead or fmShareDenyWrite);
+  try TheResponse.ContentType := 'image/png';
+    TheResponse.SetCustomHeader('Content-Disposition', 'inline; filename="images.jpg"');
+    TheResponse.SendContent;
+  finally
+    TheResponse.ContentStream.Free;
+    TheResponse.ContentStream := nil;
+  end;
+end;
+
+initialization
+  TMyAction.Register('*');
+
+end.
